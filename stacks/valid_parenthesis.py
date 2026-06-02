921de758f43286fe9e35e13d0163067e44396efd
avinashtrivedix <avinashtrivedi.x@gmail.com>
2026-06-02 11:44:04 +0530

    feat: implement stack pattern for valid parentheses verification

---
+++ stacks/valid_parenthesis.py
@@ -0,0 +1,13 @@
+def valid_parenthesis(s: str) -> bool:
+    stack = []
+    mapping = {")": "(", "}" : "{" , "]" : "["}
+    for char in s:
+        if char in mapping: # if it's a closing bracket
+            if stack and stack[-1] == mapping[char]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(char)
+
+    return len(stack) == 0
